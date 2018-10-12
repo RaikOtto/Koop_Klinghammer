a978b2a94b0fdf1064099a472f5978167dcc4445
raikotto <fubioinf@gmail.com>
2018-10-12 16:10:14 +0200

    update

--- Scripts/Publication_plots.R
+++ Scripts/Publication_plots.R
@@ -16,6 +16,7 @@ expr_raw = read.table(
   #row.names = F
 )
 colnames(expr_raw ) = str_replace_all(colnames(expr_raw), pattern = "^X", "")
+
 # variance selection
 hgnc_list = expr_raw[,1]
 hgnc_list_uni = unique(expr_raw[,1])
@@ -42,24 +43,22 @@ aka3 = list(
 ###
 
 ## Figure 1
-meta_data = read.table("~/Generic_mRNA_Expression_Pipeline/Project_files/Klinghammer_neun/RTV.tsv",sep ="\t", stringsAsFactors = F)
-colnames(meta_data) = meta_data[1,]
-meta_data = meta_data[-1,]
+meta_data = read.table("~/Generic_mRNA_Expression_Pipeline/Project_files/Klinghammer_neun/RTV.tsv",sep ="\t", stringsAsFactors = F, header = T)
 rownames(meta_data) = meta_data$ID
+meta_data = meta_data[meta_data$ID %in% colnames(expr_raw),]
 meta_data$Copanlisib  = str_replace_all(meta_data$Copanlisib, "CTRL", "Responder")
 meta_data$Copanlisib  = str_replace_all(meta_data$Copanlisib, "CASE", "Resistant")
 meta_data$Cetuximab  = str_replace_all(meta_data$Cetuximab, "CTRL", "Responder")
 meta_data$Cetuximab  = str_replace_all(meta_data$Cetuximab, "CASE", "Resistant")
-meta_data$Copan_Cetux  = str_replace_all(meta_data$Copan_Cetux, "CTRL", "Responder")
-meta_data$Copan_Cetux  = str_replace_all(meta_data$Copan_Cetux, "CASE", "Resistant")
 meta_data$Copanlisib_val = log2(as.double(str_replace(meta_data$Copanlisib_val, pattern = ",", "."))+1)
 meta_data$Cetuximab_val = log2(as.double(str_replace(meta_data$Cetuximab_val, pattern = ",", "."))+1)
-meta_data$Copan_Cetux_val = log2(as.double(str_replace(meta_data$Copan_Cetux_val, pattern = ",", "."))+1)
 
 sub_tab = read.table(
-    #"/home/ottoraik/Generic_mRNA_Expression_Pipeline/Project_files/Output/Results_Klinghammer_neun/Dif_Copan_Cetux.tsv",
-    #"/home/ottoraik/Generic_mRNA_Expression_Pipeline/Project_files/Output/Results_Klinghammer_neun/Dif_Cetuximab.tsv",
-    "/home/ottoraik/Generic_mRNA_Expression_Pipeline/Project_files/Output/Results_Klinghammer_neun/Dif_Copanselib.tsv",
+    stringr::str_replace(
+      "/home/ottoraik/Generic_mRNA_Expression_Pipeline/Project_files/Output/Results_Klinghammer_neun/dif_Copanlisib.tsv",
+      pattern = "Copanlisib",
+      cohorts_type
+    ),
     sep = "\t",
     stringsAsFactors = F,
     header = T
@@ -70,39 +69,37 @@ cor_mat = cor(expr_sub);pcr = prcomp(t(cor_mat))
 #"Copan_Cetux","Cetuximab","Copanlisib"
 pheatmap::pheatmap(
   cor_mat,
-  #annotation_col = meta_data[c("Copan_Cetux")],
-  annotation_col = meta_data[c("Copanlisib","Copanlisib_val")],
+  annotation_col = meta_data[c(cohorts_type)],
   annotation_colors = aka3,
   show_rownames = F,
   show_colnames = T,
   #treeheight_col = 0,
   legend = F,
-  fontsize_col = 7#,
-  #clustering_method = "ward.D2"
+  fontsize_col = 7,
+  clustering_method = "ward.D2"
 )
 
 ### Figure 2
 
 ggbiplot::ggbiplot(
-  pcr,
-  #groups = as.character(meta_data$Copanlisib),
-  groups = as.character(meta_data$Copan_Cetux),
-  ellipse = TRUE,
-  circle = TRUE,
-  var.axes = F,
-  #var.scale = ,
-  labels = meta_data$ID
+    pcr,
+    groups = as.character(unlist(meta_data[cohorts_type])),
+    #groups = as.character(meta_data$Copan_Cetux),
+    ellipse = TRUE,
+    circle = TRUE,
+    var.axes = F,
+    labels = meta_data$ID
 )#  + geom_point( aes( size = as.double(meta_data$OS)**2, color = as.factor(meta_data_tmp$Subtype) ))
 
 aggregate(meta_data$OS, FUN = mean, by = list(meta_data$Subtype))
 
 ### umap
 
-umap_plot = umap::umap(t(pure_data))
+umap_plot = umap::umap(t(expr))
 vis_data = as.data.frame(umap_plot$layout)
 colnames(vis_data) = c("x","y")
 dist_mat = dist((vis_data))
-p = ggplot2::qplot( x = vis_data$x, y = vis_data$y, color = meta_data$Subtype)
+p = ggplot2::qplot( x = vis_data$x, y = vis_data$y, color = (meta_data[cohorts_type]))
 p
 
 ## km plots
@@ -190,3 +187,34 @@ pheatmap::pheatmap(
 write.table(cor_t, "~/Koop_Klinghammer/Results/Klinghammer_Neun_11_10_2018/Drug_Exp_Correlations.tsv",sep="\t", quote =F, row.names = T)
 
 expr_raw["TIMD4",]
+
+## MEN1 transcripts
+
+library(ggplot2)
+
+meta_data$CDKN1B = rep("",nrow(meta_data))
+meta_data$CDKN1B = as.double(expr_raw["CDKN1B", meta_data$ID])
+  
+vis_mat = reshape2::melt( meta_data[,c("ID","CDKN1B","Copanlisib","Copanlisib_val")], id = c("ID","Copanlisib","CDKN1B","Copanlisib_val"))
+vis_mat = vis_mat[order(vis_mat$CDKN1B),]
+
+# Basic barplot
+label_vec = meta_data[rownames(vis_mat),"Copanlisib"]
+label_vec = label_vec[order(meta_data$CDKN1B)]
+label_vec[label_vec == "Resistant"] = "R"
+label_vec[label_vec != "R"] = "S"
+col_vec = label_vec
+col_vec[col_vec == "S"] = "darkgreen"
+col_vec[col_vec != "darkgreen"] = "darkred"
+
+vis_mat$ID = factor(vis_mat$ID, levels = vis_mat$ID)
+vis_mat$Copanlisib_val = log(vis_mat$Copanlisib_val)
+
+p = ggplot( data = vis_mat)
+p = p + geom_bar(aes( x = ID, y = CDKN1B, fill = Copanlisib_val ),stat="identity", colour="black")
+p = p + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+p = p + scale_fill_gradientn(colours = c("white","yellow","red"), breaks = c(0.0,.5,1.0))
+p = p + annotate("text", x=1:nrow(vis_mat),y = 12,parse=TRUE, label = label_vec, color = col_vec, size = 4.5 )
+p = p + annotate("text", x=5,y = 10,parse=TRUE, label = "pearson-correlation: -0.66", color = "black", size = 4.5 )
+p = p + xlab("") + ylab("CDKN1B expression versus Copalisib") + theme(legend.position = "top")
+p
