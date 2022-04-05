3d1ce48f3570e6dc7837c54b5e678774c4b029c0
RaikOtto <raik.otto@hu-berlin.de>
2022-04-05 08:55:22 +0200

    update

--- Scripts/Survival_analyses.R
+++ Scripts/Survival_analyses.R
@@ -5,11 +5,11 @@ library("stringr")
 
 
 expr_raw = read.table(
-  "~/Koop_Klinghammer/Data/S108 (copy 1).tsv",
+  "~/Koop_Klinghammer/Data/S103.tsv",
   sep ="\t",
   stringsAsFactors = F,
-  header = T
-  #row.names = F
+  header = T,
+  row.names = 1
 )
 colnames(expr_raw ) = str_replace_all(colnames(expr_raw), pattern = "^X", "")
 expr_raw[1:5,1:5]
@@ -19,12 +19,13 @@ dim(expr_raw)
 
 ## Figure 1
 
-meta_info = read.table("~/Koop_Klinghammer/Misc/Meta_information.tsv",sep ="\t", stringsAsFactors = F, header = T)
-rownames(meta_info) = meta_info$Sample_ID
+meta_info = read.table("~/Koop_Klinghammer/Misc/Masterliste.tsv",sep ="\t", stringsAsFactors = F, header = T)
+rownames(meta_info) = meta_info$SampleID
 
 meta_data = meta_info[colnames(expr_raw),]
-meta_data = meta_data %>% filter(Survivalstatistik_neuestes_Sample == 1)
-meta_data$Survivalstatistik_neuestes_Sample
+meta_data = meta_data %>% filter(as.character(Survivalstatistik_neuestes_Sample) == "1")
+length(meta_data$Survivalstatistik_neuestes_Sample)
+dim(meta_data)
 
 meta_data_vis = meta_data %>% filter(Arm_codiert == 1)
 meta_data_vis = meta_data %>% filter(Arm_codiert == 2)
@@ -35,6 +36,7 @@ meta_data_vis = meta_data_vis %>% filter(Subtype %in% c("MS","CL"))
 ### OS_ab_ED
 
 meta_data_vis_os = meta_data_vis %>% filter(!is.na(OS_Monate))
+meta_data_vis_os = meta_data_vis %>% filter(!is.na(PFS_Monate_ab_Einschluss))
 
 fit_os = survival::survfit( survival::Surv( as.double(OS_ab_ED), censor_OS ) ~ Subtype, data = meta_data_vis_os)
 survminer::surv_pvalue(fit_os, data = meta_data_vis_os)$pval
@@ -48,7 +50,7 @@ print(survminer::ggsurvplot(fit_os, data = meta_data_vis_os, risk.table = F, pva
 
 # PFS_ab_ED PFS_Monate_ab_Einschluss PFS_Monate
 
-meta_data_vis_pfs = meta_data_vis %>% filter(!is.na(PFS_Monate))
+meta_data_vis_pfs = meta_data_vis %>% filter(!is.na(meta_data_vis$PFS_Monate_ab_Einschluss))
 
 ## PFS_ab_ED
 
@@ -58,7 +60,15 @@ print(survminer::ggsurvplot(fit_pfs, data = meta_data_vis_pfs, risk.table = F, p
 
 ## PFS_Monate_ab_Einschluss
 
-fit_pfs = survival::survfit( survival::Surv( as.double(PFS_Monate_ab_Einschluss), censor_PFS ) ~ Subtype, data = meta_data_vis_pfs)
+schwellert = meta_data_vis_pfs$Budding_1HPF
+schwellert[schwellert %in% c(0,1)] = 0
+schwellert[schwellert > 1 ] = 1
+
+schwellert = meta_data_vis_pfs$Stroma_vitalerTumor
+schwellert[schwellert < 31] = 0
+schwellert[schwellert >= 31 ] = 1
+
+fit_pfs = survival::survfit( survival::Surv( as.double(meta_data_vis_pfs$PFS_Monate_ab_Einschluss), censor_PFS ) ~ schwellert, data = meta_data_vis_pfs)
 survminer::surv_pvalue(fit_pfs, data = meta_data_vis_pfs)$pval
 print(survminer::ggsurvplot(fit_pfs, data = meta_data_vis_pfs, risk.table = F, pval = T, censor.size = 10))
 
