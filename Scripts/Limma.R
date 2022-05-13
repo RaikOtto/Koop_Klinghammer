c5528aaf3e9b6791ca0b95a12b27fc324b72f980
ottoraik <raik.otto@hu-berlin.de>
2022-05-13 08:32:58 +0200

    update

--- Scripts/Limma.R
+++ Scripts/Limma.R
@@ -1,27 +1,49 @@
 library("stringr")
 library("limma")
+library("ggplot2")
+library("stringr")
+library("grid")
+library("umap")
 
-mean_os = mean(meta_data$OS)
-above_avg_os = meta_data$OS > mean_os
+source("~/Koop_Klinghammer/Scripts/Visualization_colors.R")
 
-design <- model.matrix(~0 + as.factor(above_avg_os))
+expr_raw = read.table(
+  "~/Koop_Klinghammer/Data/S103.tsv",
+  sep ="\t",
+  stringsAsFactors = F,
+  header = T,
+  row.names = 1
+)
+colnames(expr_raw ) = str_replace_all(colnames(expr_raw), pattern = "^X", "")
+expr_raw[1:5,1:5]
+dim(expr_raw)
+
+## Figure 1
+meta_info = read.table("~/Koop_Klinghammer/Misc/Meta_information.tsv",sep ="\t", stringsAsFactors = F, header = T)
+rownames(meta_info) = meta_info$Sample_ID
+
+cor_mat = cor(expr_raw);pcr = prcomp(t(cor_mat))
+matcher = match(as.character(colnames(cor_mat)), as.character(meta_info$SampleID), nomatch = 0)
+meta_data = meta_info[matcher,]
+rownames(meta_data) = meta_data$SampleID
+
+design <- model.matrix(~0 + meta_data$Subtype)
 #design <- model.matrix(~0 + as.factor(meta_data$Subtype))
-#colnames(design) = c("BA","CL","MS")
-colnames(design) = c("Not_above_avg_OS","Above_avg_OS")
+colnames(design) = c("BA","CL","MS")
 
 #vfit <- lmFit(t(r_mat[,c(-1,-2)]),design)
-vfit <- lmFit(pure_data,design)
+vfit <- lmFit(expr_raw,design)
 #vfit <- contrasts.fit(vfit, contrasts=contr.matrix)
 #plotSA(efit)
 
 #contr.matrix = makeContrasts( contrast = CL - MS,  levels = design )
-contr.matrix = makeContrasts( contrast = Not_above_avg_OS - Above_avg_OS ,  levels = design )
+contr.matrix = makeContrasts( contrast = BA - CL ,  levels = design )
 vfit <- contrasts.fit( vfit, contrasts = contr.matrix)
 efit <- eBayes(vfit)
 
 summary(decideTests(efit))
 
-result_t = topTable( efit, coef = "contrast", number  = nrow(pure_data), adjust  ="none", p.value = 1, lfc = 0)
+result_t = topTable( efit, coef = "contrast", number  = nrow(expr_raw), adjust  ="none", p.value = 1, lfc = 0)
 result_t$hgnc_symbol = rownames(result_t)
 colnames(result_t) = c("Log_FC","Average_Expr","t","P_value","adj_P_value","B","HGNC")
 
@@ -35,19 +57,3 @@ result_t = result_t[order(result_t$Log_FC,decreasing = T),]
 
 ###
 
-centroid_mat = read.table("~/Koop_Klinghammer/Misc/centroids of 821 predictive gene signatures.tsv",sep ="\t", header = T, stringsAsFactors = F)
-centroid_mat$IMS = as.double(str_replace_all(centroid_mat$IMS, pattern = ",", "."))
-centroid_mat$BA = as.double(str_replace_all(centroid_mat$BA, pattern = ",", "."))
-centroid_mat$CL = as.double(str_replace_all(centroid_mat$CL, pattern = ",", "."))
-
-r_match = match( str_to_upper(centroid_mat$GENE), str_to_upper(result_t$HGNC),  nomatch = 0)
-rev_match = match( str_to_upper(result_t$HGNC),str_to_upper(centroid_mat$GENE),  nomatch = 0)
-
-add_mat = matrix( rep("", 3 * nrow(result_t)), ncol = 3 )
-add_mat[r_match , 1] = centroid_mat[ r_match != 0, 2]
-add_mat[r_match , 2] = centroid_mat[ r_match != 0, 3]
-add_mat[r_match , 3] = centroid_mat[ r_match != 0, 4]
-
-result_t = cbind(result_t,add_mat )
-
-#write.table("~/Koop_Klinghammer/Results/Linear_regression/BA_vs_all.tsv", x = result_t, sep = "\t", quote = F, row.names = F)
